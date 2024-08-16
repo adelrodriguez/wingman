1e26a984b853fa87a033fa242a124ee1bc3467a7
Adel Rodríguez <hello@adelrodriguez.com>
2024-08-16 10:28:39 -0400

    feat: add until util

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,1 +1,29 @@
 export class InternalError extends Error {}
+
+/**
+ * Gracefully handle errors as values.
+ *
+ * @example
+ * ```ts
+ * const [error, data] = await until(() => asyncAction())
+ *
+ * if (error) {
+ *   // handle error
+ *   return
+ * }
+ *
+ * // data is now the response from asyncAction
+ * ```
+ */
+export async function until<
+  F extends (...args: unknown[]) => Promise<unknown>,
+  E extends Error = Error,
+>(promise: F): Promise<[null, Awaited<ReturnType<F>>] | [E, null]> {
+  try {
+    const data = await promise()
+
+    return [null, data as Awaited<ReturnType<F>>]
+  } catch (error) {
+    return [error as E, null]
+  }
+}
